d1e499c9ad5f3846b59af933a8396294e866b06d
Roman Masliak <maslyak3@gmail.com>
2025-04-24 14:16:48 +0300

    feat: reset main branch

--- client-chat/src/index.ts
+++
@@ -1,7 +0,0 @@
-import { initRouter } from './router/router';
-import { renderLoginPage } from './views/login';
-import { renderAboutPage } from './views/about';
-import { renderChatPage } from './views/chat';
-import './style.css';
-
-initRouter();
